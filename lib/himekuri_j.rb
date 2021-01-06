9fb1f2e9c37b6f20504b993019a370f9c1087f91
Takayuki Kamiyama <karuma.reason@gmail.com>
2021-01-06 16:19:13 +0900

    Update.

--- lib/himekuri_j.rb
+++ lib/himekuri_j.rb
@@ -3,22 +3,16 @@
 lib = File.expand_path('../lib', __FILE__)
 $LOAD_PATH.unshift(lib) unless $LOAD_PATH.include?(lib)
 
-require 'open3'
 require "himekuri_j/version"
 
 class HimekuriJBasic
   def self.before
-    # vesion info
-    ruby_version = (RUBY_VERSION).to_s
-    version = (HimekuriJ::VERSION).to_s
-    himekuri_j = "himekuri_j-".to_s + version.to_s
-    java_path= ("/Library/Java/JavaVirtualMachines/adoptopenjdk-8.jdk/Contents/Home/bin/java").to_s
     
-    javalang_path = java_path "$HOME/.rbenv/versions/" + ruby_version + "/lib/ruby/gems/" + ruby_version + "/gems/" + himekuri_j + "/lib/HimekuriJ.class".to_s
-    stdout_java, stderr_java, status_java = Open3.capture3(javalang_path)
+    begin
+    system('java HimekuriJ')
     
-    stdout_java
+    rescue Exception => e
+    puts e.backtrace
+    end
   end
 end
-
-puts HimekuriJBasic.before
